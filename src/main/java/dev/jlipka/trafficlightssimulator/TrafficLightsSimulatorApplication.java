5b566dfeacc7ec065268477d8ddf15bcf97c1600
Moher <47338928+newmon13@users.noreply.github.com>
2025-05-12 15:32:02 +0200

    Initial commit

---
+++ src/main/java/dev/jlipka/trafficlightssimulator/TrafficLightsSimulatorApplication.java
@@ -0,0 +1,13 @@
+package dev.jlipka.trafficlightssimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficLightsSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficLightsSimulatorApplication.class, args);
+	}
+
+}
